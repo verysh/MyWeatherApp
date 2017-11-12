98a14c59b88b2285e8444aabf71e5287a5761eac
Vladimir Eryshev <vladimir.eryshev@wukkerteam.com>
2017-11-12 16:23:49 +0300

    Initial Commit

---
+++ MyWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyWeatherApp
+//
+//  Created by VLAD on 12/11/2017.
+//  Copyright © 2017 Vlad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
